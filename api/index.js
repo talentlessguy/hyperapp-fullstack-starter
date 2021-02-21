317932e3dc90e38492dc907540cdb54aa594d158
v1rtl <pilll.pl22@gmail.com>
2021-02-21 18:58:31 +0300

    lil fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 import { createApp } from '../server.js'
 
-module.exports = async (req, res) => (await createApp()).handler
+module.exports = async (req, res) => (await createApp()).handler(req, res)
